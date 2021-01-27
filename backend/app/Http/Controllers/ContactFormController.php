d2da0875a2f3ffa3bc14a8a2bd71c8d19cab3213
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-01-27 23:00:24 +0900

    検索フォームを追加した

--- backend/app/Http/Controllers/ContactFormController.php
+++ backend/app/Http/Controllers/ContactFormController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ContactForm;
+use App\Services\CheckFormData;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\StoreContactForm;
 
 class ContactFormController extends Controller
 {
@@ -11,9 +15,32 @@ class ContactFormController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('contact.index');
+        $search = $request->input('search');
+        // $contacts = ContactForm::all();
+
+        // $contacts = DB::table('contact_forms')
+        //     ->select('id', 'your_name', 'title', 'created_at')
+        //     ->orderBy('created_at', 'desc')
+        //     ->paginate(10);
+
+        $query = DB::table('contact_forms');
+
+        if ($search !== null) {
+            $search_split = mb_convert_kana($search, 's');
+            $search_split2 = preg_split('/[\s]+/', $search_split, -1, PREG_SPLIT_NO_EMPTY);
+
+            foreach ($search_split2 as $value) {
+                $query->where('your_name', 'like', '%' . $value . '%');
+            }
+        }
+
+        $query->select('id', 'your_name', 'title', 'created_at');
+        $query->orderBy('created_at', 'asc');
+        $contacts = $query->paginate(20);
+
+        return view('contact.index', compact('contacts'));
     }
 
     /**
